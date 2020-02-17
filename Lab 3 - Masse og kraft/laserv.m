46bf7751499b0d898e56d3284d06d350881f28a7
Vegard Falmaar <vegardfa@student.matnat.uio.no>
2020-02-17 11:52:18 +0100

    More journal

--- Lab 3 - Masse og kraft/laserv.m
+++ Lab 3 - Masse og kraft/laserv.m
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
-masser = [10 100 110 500 1000 1010 2000 2010];
-utslag = [16 39 43 185 375 383 740 745];
+masser = [0 10 100 110 500 1000 1010 2000 2010];
+utslag = [0 6 39 43 185 375 383 740 745];
 
-fit = fitlm(masser(1:6), utslag(1:6))
+% fit = fitlm(masser(1:7), utslag(1:7));
+fit = fitlm(masser, utslag);
 
 hold on
 plot(masser, utslag, 'o')
@@ -10,21 +10,28 @@ plot(fit)
 hold off
 
 coeffs = fit.Coefficients.Estimate;
-a = coeffs(2)
-b = coeffs(1)
-
-m = (utslag(7) - b)/a
-dm = sqrt((5/a)^2 + (3.1937/a)^2 + (m*0.005167/a)^2)
+a = coeffs(2);
+b = coeffs(1);
+Errors = fit.Coefficients.SE;
+da = 2*Errors(2);
+db = 2*Errors(1);
 
 m = (utslag(8) - b)/a
-dm = sqrt((5/a)^2 + (3.1937/a)^2 + (m*0.005167/a)^2)
-=======
- mass = [0.01, 0.1, 0.11, 0.5, 1, 1.01, 2, 2.01]; 
- 
- length = [16, 39, 43, 185, 375, 383, 740, 745];
- 
- scatter(mass, length)
+dm = sqrt((5/a)^2 + (db/a)^2 + (m*da/a)^2)
 
-xlabel("masse [kg]")
-ylabel("utslag [mm]")
->>>>>>> 47005446b8d9060212d38435064d34742ca14f4a
+m = (utslag(9) - b)/a
+dm = sqrt((5/a)^2 + (db/a)^2 + (m*da/a)^2)
+
+m = (380 - b)/a
+dm = sqrt((3/a)^2 + (db/a)^2 + (m*da/a)^2)
+
+% fit = fitlm(masser, utslag);
+m = (760 - b)/a
+dm = sqrt((5/a)^2 + (db/a)^2 + (m*da/a)^2)
+
+% fit = fitlm(masser(1:4), utslag(1:4));
+m = (40 - b)/a
+dm = sqrt((1/a)^2 + (db/a)^2 + (m*da/a)^2)
+
+m = (190 - b)/a
+dm = sqrt((2/a)^2 + (db/a)^2 + (m*da/a)^2)
