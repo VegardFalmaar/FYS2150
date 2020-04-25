0d3c65a3e04d6716aeadd3566c34f60620fea818
Kamilla Sulebakk <kisuleba@student.matnat.uio.no>
2020-04-25 15:06:42 +0200

    Prelab 9 finished

---
+++ Lab 9 - Polarisasjon/prelab.m
@@ -0,0 +1,12 @@
+for i = 1:1:3
+    filename = ['data/polarisering' string(i) '.dat'];
+    filename = join(filename, '');
+    data = importdata(filename);
+    E_y = data(:, 1);
+    E_z = data(:, 2);
+    figure()
+    hold on
+    plot(E_y(1:500))
+    plot(E_z(1:500))
+    legend(['E_y'; 'E_z'])
+end
